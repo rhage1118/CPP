ebf32bc4e35d82ecca5cb2bc99e52ded714c404f
Ramsey Hage <ramsey.hage@gmail.com>
2021-10-21 22:52:41 -0400

    Initial Commit

---
+++ XCode_CPP/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  XCode_CPP
+//
+//  Created by Ramsey Hage on 2021-10-21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
